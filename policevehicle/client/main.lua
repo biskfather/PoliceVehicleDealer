dd846e687c14012e6d7ccdeea1e88cdf6fe2955f
biskfather <80595080+biskfather@users.noreply.github.com>
2026-09-22 13:03:04 +0100

    Add files via upload

---
+++ policevehicle/client/main.lua
@@ -0,0 +1,149 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local lib = exports.ox_lib  
+
+local dealerPed = nil
+
+local targetOption = {
+    name = 'policevehicle_dealer',
+    icon = 'fa-solid fa-car',
+    distance = 2.0
+}
+
+CreateThread(function()
+    local npc = Config.NPC
+    RequestModel(npc.model)
+    while not HasModelLoaded(npc.model) do
+        Wait(10)
+    end
+
+    local ped = CreatePed(4, npc.model, npc.coords.x, npc.coords.y, npc.coords.z - 1, npc.coords.w, false, true)
+    FreezeEntityPosition(ped, true)
+    SetEntityInvincible(ped, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    SetModelAsNoLongerNeeded(npc.model)
+
+    dealerPed = ped
+
+    Target.addLocalEntity(ped, {
+        name = targetOption.name,
+        icon = targetOption.icon,
+        label = npc.label,
+        distance = npc.targetDistance or targetOption.distance,
+        onSelect = function()
+            QBCore.Functions.TriggerCallback('rs-policecars:getAvailableVehicles', function(vehicles)
+                TriggerEvent('rs-policecars:openMenu', vehicles)
+            end)
+        end
+    })
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource ~= GetCurrentResourceName() then return end
+
+    if dealerPed and DoesEntityExist(dealerPed) then
+        Target.removeLocalEntity(dealerPed, {
+            name = targetOption.name,
+            label = Config.NPC.label
+        })
+        DeleteEntity(dealerPed)
+    end
+
+    dealerPed = nil
+end)
+
+RegisterNetEvent('rs-policecars:confirmBuy', function(vehicleModel)
+    exports.ox_lib:registerContext({
+        id = "vehicle_purchase_confirm",
+        title = "Confirm Purchase",
+        options = {
+            {
+                title = "Yes, buy the vehicle again",
+                description = "You already have this vehicle, are you sure?",
+                event = "rs-policecars:buyVehicleConfirmed",
+                args = vehicleModel
+            },
+            {
+                title = "No, cancel",
+                description = "You already have this vehicle.",
+                event = "rs-policecars:cancelPurchase"
+            }
+        }
+    })
+
+    exports.ox_lib:showContext("vehicle_purchase_confirm")
+end)
+
+RegisterNetEvent('rs-policecars:buyVehicleConfirmed', function(vehicleModel)
+    TriggerServerEvent('rs-policecars:buyVehicleConfirmed', vehicleModel)
+end)
+
+RegisterNetEvent('rs-policecars:cancelPurchase', function()
+    TriggerEvent('QBCore:Notify', 'Purchase canceled.', 'error')
+end)
+
+
+RegisterNetEvent('rs-policecars:buyVehicle', function(vehicleModel)
+    
+
+
+    if vehicleModel then
+        TriggerServerEvent('rs-policecars:buyVehicle', vehicleModel)
+    else
+        print("No vehicle model provided!")
+    end
+end)
+
+RegisterNetEvent('rs-policecars:spawnVehicle', function(vehicleModel, spawnCoords, plate)
+    print("Try to spawn vehicle: " .. vehicleModel)
+
+    local playerPed = PlayerPedId()
+
+    RequestModel(vehicleModel)
+    while not HasModelLoaded(vehicleModel) do
+        Wait(500)
+    end
+
+   
+
+    
+    local vehicle = CreateVehicle(vehicleModel, spawnCoords.x, spawnCoords.y, spawnCoords.z, spawnCoords.w, true, false)
+
+    if vehicle and DoesEntityExist(vehicle) then
+        
+
+        SetVehicleFuelLevel(vehicle, 100.0)
+
+        TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
+
+        SetVehicleNumberPlateText(vehicle, plate)
+        
+
+    else
+        print("Something went wrong when spawning the vehicle!")
+    end
+end)
+
+RegisterNetEvent('rs-policecars:openMenu', function(vehicles)
+    local options = {}
+
+    for _, vehicle in pairs(vehicles) do
+        table.insert(options, {
+            title = vehicle.label,
+            description = "Price: $" .. vehicle.price,
+            event = "rs-policecars:buyVehicle",
+            args = vehicle.model
+        })
+    end
+
+    if #options > 0 then
+        exports.ox_lib:registerContext({
+            id = "police_vehicle_menu",
+            title = "Police Vehicles",
+            options = options
+        })
+
+        exports.ox_lib:showContext("police_vehicle_menu")
+    else
+        TriggerEvent('QBCore:Notify', 'No vehicles available for your rank.', 'error')
+    end
+end)
---
+++ policevehicle/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dean GM'
+description 'policevehicle Dealership'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server/main.lua'
+
+client_scripts {
+    'client/target.lua',
+    'client/main.lua'
+}
+
+dependency 'qb-core'
+dependency 'ox_lib'
+
+-- Also requires a target resource: ox_target (https://github.com/TheOrderFivem/ox_target)
+-- or qb-target. See Config.Target in config.lua.
